78a9ed25d31e15983cf572cb180cec3e7f3633e0
Alano Acioli <alanoacioli@gmail.com>
2019-09-27 09:56:07 -0300

    Change function_setup to setUp and function_teardown to tearDown
    Fix functions name that execute before and after run tests

--- Python/basic_fixture/factorial_test.py
+++ Python/basic_fixture/factorial_test.py
@@ -5,11 +5,11 @@ class TestFactorial(unittest.TestCase):
 
   fact = Factorial()
 
-  def setup_function(self):
+  def setUp(self):
       # The f_setup() function run before test
-      print('This function will be triggered on the start of the test')
+      print('\nThis function will be triggered on the start of the test')
 
-  def teardown_function(self):
+  def tearDown(self):
       # The f_teardown() function removes the yes.txt file, if it was created.
       print('This function will be triggered on the end of the test')
 
