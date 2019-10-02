2a4d681d94c006da739489671a121eaede090dd3
Alano Acioli <alanoacioli@gmail.com>
2019-10-02 11:03:44 -0300

    Add ros testing for python

---
+++ Python/python_factorial/test/test_factorial.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# import roslib; roslib.load_manifest(PKG) # This line is not needed with Catkin.
+
+import sys
+# sys.path.insert(1, '/path/to/application/app/scripts')
+import unittest
+from python_factorial import SubscriberTool
+
+# from python_factorial import listener
+import listener
+
+
+# package = 'python_factorial'
+class TestFactorial(unittest.TestCase):
+
+  def setUp(self):
+      # The f_setup() function run before test
+    #   rospy.init_node('listener', anonymous=True)
+      print('\nThis function will be triggered on the start of the test')
+
+  def tearDown(self):
+      # The f_teardown() function removes the yes.txt file, if it was created.
+      print('This function will be triggered on the end of the test')
+
+  def test_zero(self):
+      self.assertEqual(listener.factorial(0),1)
+
+  # def test_positive(self):
+  #     self.assertEqual(listener.factorial(1),1)
+  #     self.assertEqual(listener.factorial(2),2)
+  #     self.assertEqual(listener.factorial(3),6)
+  #     self.assertEqual(listener.factorial(8),40320)
+
+PKG = 'python_factorial'
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun(PKG, 'test_factorial', TestFactorial)
+    # unittest.main()
