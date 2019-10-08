976c7fbb05cf780e5fdc0e8b95329ff91c435653
Alano Acioli <alanoacioli@gmail.com>
2019-10-08 18:53:29 -0300

    Add basic_communication folder
    Configure setup.py to find basic_communication as a package
    Configure test to verify chatter topic output
    Configure test to verify listerner topic output

--- Python/python_factorial/setup.py
+++ Python/python_factorial/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['python_factorial'],
+    packages=['basic_communication'],
     package_dir={'': 'scripts'},
 )
 
--- Python/python_factorial/test/test_factorial.py
+++ Python/python_factorial/test/test_factorial.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python
 
-# import roslib; roslib.load_manifest(PKG) # This line is not needed with Catkin.
-
 import sys
-# sys.path.insert(1, '/path/to/application/app/scripts')
 import unittest
 import rospy
 
 from std_msgs.msg import Int64
+from basic_communication import ListenerFactorial
 
-from python_factorial import ListenerFactorial
-
-
-# package = 'python_factorial'
 class TestFactorial(unittest.TestCase):
 
-  def setUp(self):
-      # The f_setup() function run before test
-      rospy.init_node('listener', anonymous=True)
-      print('\nThis function will be triggered on the start of the test')
+    def setUp(self):
+        # The f_setup() function run before test
+        rospy.init_node('listener', anonymous=True)
+        print('\nThis function will be triggered on the start of the test')
 
-  def tearDown(self):
-      # The f_teardown() function removes the yes.txt file, if it was created.
-      print('This function will be triggered on the end of the test')
+    def tearDown(self):
+        # The f_teardown() function removes the yes.txt file, if it was created.
+        print('This function will be triggered on the end of the test')
 
-  def test_zero(self):
-      # sub = ListenerFactorial('chatter', Int64)
-      # self.assertEqual(sub.factorial(0),1)
-      self.assertEqual(1,1)
+    def test_factorial_message(self):
+        listen = ListenerFactorial('chatter', Int64)
+        rospy.sleep(2)
+        self.assertEqual(listen.message(),120)
 
-  # def test_positive(self):
-  #     self.assertEqual(listener.factorial(1),1)
-  #     self.assertEqual(listener.factorial(2),2)
-  #     self.assertEqual(listener.factorial(3),6)
-  #     self.assertEqual(listener.factorial(8),40320)
+    def test_topic_message(self):
+        listen = ListenerFactorial('chatter', Int64)
+        rospy.sleep(2)
+        self.assertEqual(listen.listened(),5)
+
 
 PKG = 'python_factorial'
 if __name__ == '__main__':
